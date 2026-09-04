1074f5ba2fd30fe4e9c2bf407d7cf8079145bfc3
KingaSzayer-Iths <kinga.szayer@iths.se>
2026-09-04 16:27:12 +0200

    style responsive footer and add to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
 
 const geist = Geist({
   variable: "--font-geist",
@@ -27,6 +28,7 @@ export default function RootLayout({
         </a>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
