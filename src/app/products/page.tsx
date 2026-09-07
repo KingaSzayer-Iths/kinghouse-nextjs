148141ef5d1ba191760422247a841c6d89aac97c
KingaSzayer-Iths <kinga.szayer@iths.se>
2026-09-07 09:58:33 +0200

    display all products on products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,18 @@
-export default function ProductsPage() {
+import ProductCard from "@/components/ProductCard/ProductCard";
+import { getProducts } from "@/lib/products";
+
+export default async function ProductsPage() {
+  // Hämtar alla produkter från mock-API
+  const products = await getProducts();  
   return (
     <main id="main-content">
-      {/* Tillfälligt innehåll tills produktsidan byggs */}
       <h1>Produkter</h1>
+      <div className="productGrid">
+        {/* Hela sortimentet ska visas */}
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </main>
   );
 }
