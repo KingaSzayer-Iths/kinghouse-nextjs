a1c4d21181818d7138177d5a0e7427860f092436
KingaSzayer-Iths <kinga.szayer@iths.se>
2026-09-07 11:22:25 +0200

    style responsive products grid

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,13 +1,15 @@
 import ProductCard from "@/components/ProductCard/ProductCard";
 import { getProducts } from "@/lib/products";
+import styles from "./page.module.css";
 
 export default async function ProductsPage() {
   // Hämtar alla produkter från mock-API
   const products = await getProducts();  
   return (
-    <main id="main-content">
-      <h1>Produkter</h1>
-      <div className="productGrid">
+  <main id="main-content" className={styles.productsPage}>
+    <h1 className={styles.title}>Produkter</h1>
+    
+      <div className={styles.productGrid}>
         {/* Hela sortimentet ska visas */}
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
