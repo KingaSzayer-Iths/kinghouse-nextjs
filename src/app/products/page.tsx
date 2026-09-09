03ef80ee249f2e93a28cff04c6a5f6e47893d7a3
KingaSzayer-Iths <kinga.szayer@iths.se>
2026-09-09 21:32:27 +0200

    clean up products page

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -4,22 +4,13 @@ import ProductList from "@/components/ProductList/ProductList";
 
 export default async function ProductsPage() {
   // Hämtar alla produkter från mock-API
-  const products = await getProducts();  
+  const products = await getProducts();
   return (
-  <main id="main-content" className={styles.productsPage}>
-    <h1 className={styles.title}>Produkter</h1>
-    
-  {/* Skickar produktdata till ProductList som visar produkterna */}  
-    <ProductList products={products} />
-    
+    <main id="main-content" className={styles.productsPage}>
+      <h1 className={styles.title}>Produkter</h1>
 
-{/* tar bort den delen: Och eftersom ProductCard då inte längre används direkt i page.tsx */}
-      {/* <div className={styles.productGrid}> */}
-        {/* Hela sortimentet ska visas */}
-        {/* {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div> */}
+      {/* Skickar produktdata till ProductList som visar produkterna */}
+      <ProductList products={products} />
     </main>
   );
 }
